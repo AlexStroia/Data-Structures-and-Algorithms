bbb215de384c12c8726d06d996f548f549b7c03e
Alexandru Stroia <alexandru.stroia@halcyonmobile.com>
2019-08-24 17:11:05 +0300

    Add implementation for bubblesort

---
+++ src/com/alex/AlgoHelper.java
@@ -0,0 +1,10 @@
+package com.alex;
+
+public class AlgoHelper {
+
+    public static void printArray(int[] array) {
+        for(int i=0;i<array.length;i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
--- src/com/alex/sortingalgorithms/BubbleSort.java
+++ src/com/alex/sortingalgorithms/BubbleSort.java
@@ -1,19 +1,21 @@
 package com.alex.sortingalgorithms;
 
+import com.alex.AlgoHelper;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
         int[] array = {20, 35, 0, 11, 10, 3, -2};
 
         bubbleSort(array);
+        AlgoHelper.printArray(array);
     }
 
     private static void bubbleSort(int[] array) {
         for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-
             for (int i = 0; i < lastUnsortedIndex; i++) {
                 if (array[i] > array[i + 1]) {
-                    swap(array, i, lastUnsortedIndex);
+                    swap(array, i, i+1);
                 }
             }
         }
